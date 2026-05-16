e8b54c166183aaac64488b88854badd1b2afe630
chris <chri.perezv@duocuc.cl>
2026-05-16 13:04:51 -0400

    Creación api-gateway

--- ms-gateway/src/main/java/com/smartlogix/gateway/GatewayConfig.java
+++ ms-gateway/src/main/java/com/smartlogix/gateway/GatewayConfig.java
@@ -13,13 +13,41 @@ import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFu
 @Configuration
 public class GatewayConfig {
 
+    // 1. Ruta para ms-inventario (Puerto 8081)
     @Bean
     public RouterFunction<ServerResponse> inventarioRoute() {
         return route("inventario_route")
-                // Se define qué rutas atrapar y se llama a http() vacío
                 .route(RequestPredicates.path("/api/productos").or(RequestPredicates.path("/api/productos/**")), http())
-                // Se define hacia dónde se redirige usando el filtro uri()
                 .before(uri("http://localhost:8081"))
+                .build();
+    }
+
+    // 2. Ruta para ms-clientes (Puerto 8082)
+    @Bean
+    public RouterFunction<ServerResponse> clientesRoute() {
+        return route("clientes_route")
+                .route(RequestPredicates.path("/api/clientes").or(RequestPredicates.path("/api/clientes/**")), http())
+                .before(uri("http://localhost:8082"))
+                .build();
+    }
+
+    // 3. Ruta para ms-ventas (Puerto 8083) - Incluye pedidos y pagos
+    @Bean
+    public RouterFunction<ServerResponse> ventasRoute() {
+        return route("ventas_route")
+                .route(RequestPredicates.path("/api/pedidos").or(RequestPredicates.path("/api/pedidos/**"))
+                        .or(RequestPredicates.path("/api/pagos")).or(RequestPredicates.path("/api/pagos/**")), http())
+                .before(uri("http://localhost:8083"))
+                .build();
+    }
+
+    // 4. Ruta para ms-logistica (Puerto 8084) - Incluye despachos y transportistas
+    @Bean
+    public RouterFunction<ServerResponse> logisticaRoute() {
+        return route("logistica_route")
+                .route(RequestPredicates.path("/api/despachos").or(RequestPredicates.path("/api/despachos/**"))
+                        .or(RequestPredicates.path("/api/transportistas")).or(RequestPredicates.path("/api/transportistas/**")), http())
+                .before(uri("http://localhost:8084"))
                 .build();
     }
 }
